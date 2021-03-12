666eb4d5ebcc6beb6a8c2ccd96fec6eb5a856e11
Mladen Terzic <terza98@hotmail.rs>
2021-03-12 13:31:59 +0100

    fetch and map orders from API + add faked notification message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+const API_URL = 'https://zbkicks.herokuapp.com/api/v1/orders/shopify_list';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [orders, setOrders] = useState();
+	const [activeOrder, setActiveOrder] = useState(0);
+
+	useEffect(() => {
+		fetch(API_URL, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(response => response.json())
+			.then(data => {
+				setOrders(data);
+			});
+	}, []);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (activeOrder === orders?.orders.length) setActiveOrder(0);
+			else setActiveOrder(activeOrder + 1);
+		}, 5000);
+		return () => clearInterval(interval);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [activeOrder]);
+
+	const calcRandomNumber = () => {
+		return Math.round(Math.random() * 10);
+	};
+
+	return (
+		<div className="App">
+			<div id="popup">
+				{orders?.orders.map(
+					(order, index) =>
+						activeOrder === index && (
+							<div className="flex" key={index}>
+								<div className="image">
+									<img src={order.image} alt="orderImage" />
+								</div>
+								<div className="orderDetails">
+									<h3 className="orderTitle">
+										{`${order.user_name} in ${order.city}, ${order.state}`}
+									</h3>
+									<p className="orderSubtitle">
+										{`Purchased ${order.product_name}`}
+									</p>
+									<p
+										style={{ color: 'red' }}
+									>{`${calcRandomNumber()} Minute Ago`}</p>
+								</div>
+							</div>
+						),
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
